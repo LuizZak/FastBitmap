e45d61b0fe7d87645fa427f8c3708e64aa9a6e87
UlyssesWu <wdwxy12345@gmail.com>
2019-09-11 00:08:02 +0800

    add netstandard support

--- FastBitmap/Properties/AssemblyInfo.cs
+++ FastBitmap/Properties/AssemblyInfo.cs
@@ -4,12 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FastBitmapLib")]
-[assembly: AssemblyDescription("A fast C# Bitmap wrapping layer")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Luiz Fernando")]
-[assembly: AssemblyProduct("FastBitmap")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4bcc21c7-7aa5-4c80-bd42-aab57d5284c1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
